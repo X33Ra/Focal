d42ef505cafcc38d87b2268bb9d38e0269ebcf1a
MrChan <mrkcwayne@gmail.com>
2023-05-31 15:55:37 -0600

    passing grade

--- Filtering/passingGradesOnly.js
+++ Filtering/passingGradesOnly.js
@@ -6,4 +6,11 @@ const grades = [73, 69, 3, 100, 50, 70, 69, 88, 95, 77, 35];
 // rules
 // Use the Array filter function (as shown previously) to print out only the passing grades.
 // Start by declaring the callback function and then later refactor it to be inline and anonymous, or start with an anonymous function to begin with.
+
+const passingGrades = grades.filter(function(grade) { // this is a anonymous function
+  return grade >= 70; // this will read the array of grades then it will return all the passing grades only
+});
+console.log("Passing grades:", passingGrades);
+
+
 // Convert the anonymous function into an arrow function in order to get some practice with them.
