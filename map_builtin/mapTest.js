fefbccb2f36b9622ce4de0ac4a29daf8cfac5b5d
MrChan <mrkcwayne@gmail.com>
2023-05-31 19:54:09 -0600

    wrote the actual code that actually runs

--- map_builtin/mapTest.js
+++ map_builtin/mapTest.js
@@ -5,8 +5,13 @@
 
 // The array of numbers should be the length of each name in the lighthouses.
 
-const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+const lighthousees = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
 
+const nameLengths = lighthousees.map(function(name) {
+  return name.length;
+});
+
+console.log(nameLengths);
 
 
 // write code here to use map
