faf3cc8c04901e7f9bd726b22ba56d908c2c1c77
MrChan <mrkcwayne@gmail.com>
2023-05-31 19:53:47 -0600

    wrote rules and starting guide function

---
+++ map_builtin/mapTest.js
@@ -0,0 +1,13 @@
+// rules
+// Use the map method on lighthouses so that we instead see an array of numbers.
+
+// Example: function (x) { return (x * 2); }
+
+// The array of numbers should be the length of each name in the lighthouses.
+
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+
+
+
+// write code here to use map
+// It should output: [ 15, 13, 11, 16, 10, 11, 10 ]
