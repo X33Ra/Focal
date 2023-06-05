5c747d9c9feb12ed4451f54d96fd4cb02585c2a6
MrChan <mrkcwayne@gmail.com>
2023-06-04 22:11:38 -0600

    added rules and framework

---
+++ swapper.js
@@ -0,0 +1,14 @@
+// Rules & Framework
+
+// In this exercise, you will write a function that will swap values between two objects. Your function will receive four parameters:
+
+// key1, a string
+// object1, an object
+// key2, a string
+// object2, an object
+// Your function should:
+
+// fetch the value stored in key1 in object1, and then store that value in key2 of object2.
+// take the original value stored in key2 of object2, and store that in key1 of object1.
+// not need to return anything.
+
